5742252d3931367d0e9763cbf93076736827c01f
Romiya Dangol <91256132+romiyadangol@users.noreply.github.com>
2024-07-31 11:22:47 +0545

    Add files via upload

---
+++ attendance.rb
@@ -0,0 +1,19 @@
+class Employee
+
+end
+
+class attendance_manager
+
+end
+
+
+loop do
+  puts "1. Add Employee"
+  puts "2. Mark Attendance"
+  puts "3. Display Attendance"
+  puts "4. Search Attendance"
+  puts "5. Delete Attendance"
+  puts "6. Exit"
+
+  puts "Enter your choice: "
+end
